7119289242643d3b4387e767dc6a8e02f561bb4c
cncPomper <62251698+cncPomper@users.noreply.github.com>
2020-04-04 01:24:35 +0200

    Update topic.py

--- topic.py
+++ topic.py
@@ -2,9 +2,9 @@ import datetime
 
 
 class Topic:
-    def __init__(self, name: str, difficulty: int):
+    def __init__(self, name: str, difficulty: int, time_spent: int):
         self.name = name
         self.difficulty = difficulty
+        self.time_spent = time_spent
         self.last_done = datetime.datetime.now()
         self.is_done = False
-
