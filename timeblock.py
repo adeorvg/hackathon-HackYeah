117fe253a38cd091090266914db1e6ecae79e4ed
Robert Komar <robkomar98@gmail.com>
2020-04-04 16:03:04 +0200

    google calendar API format

--- timeblock.py
+++ timeblock.py
@@ -9,9 +9,17 @@ class TimeBlock:
         self.is_done = False
         self.duration = duration
         
-    def finish_block(self):
+    def finish(self):
         self.is_done = True
 
     def set_time_range(self, start_time):
         self.start_time = start_time
         self.end_time = self.start_time + datetime.timedelta(minutes=self.duration)
+
+    def to_google_calendar(self):
+        GMT_OFF = "-07:00"
+        return {
+            "summary": self.name,
+            "start": {"dateTime": (self.start_time.strftime("%Y-%m-%dT%H:%M:00") + "%s") % GMT_OFF},
+            "end":  {"dateTime": (self.end_time.strftime("%Y-%m-%dT%H:%M:00") + "%s") % GMT_OFF}
+        }
