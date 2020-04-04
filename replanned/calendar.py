61adb63fa1769c3c358e9e0060d783bb13a8136c
Lasze <Laszer2710@gmail.com>
2020-04-04 21:58:33 +0200

    update

--- replanned/calendar.py
+++ replanned/calendar.py
@@ -1,18 +1,8 @@
-##############
-
 from __future__ import print_function
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-##############
-
-import category
-import datetime
-import topic
-import timeblock
-
-##############
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 store = file.Storage('storage.json')
@@ -22,8 +12,6 @@ if not creds or creds.invalid:
     creds = tools.run_flow(flow, store)
 GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
-GMT_OFF = "+01:00"
-###############
 from . import category
 from . import topic
 from . import timeblock
