76050e00d630b9cdef1090ca3a709927fbf1cf92
Lasze <Laszer2710@gmail.com>
2020-04-04 03:12:15 +0200

    update

--- topic.py
+++ topic.py
@@ -6,8 +6,3 @@ class Topic:
         self.difficulty = difficulty
         self.time_spent = time_spent
         self.last_done = datetime.datetime.now()
-<<<<<<< HEAD
-        
-=======
-        self.is_done = False
->>>>>>> 7119289242643d3b4387e767dc6a8e02f561bb4c
