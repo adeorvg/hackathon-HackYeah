7b5916ae2dfbd57da3342db7e2dcd8279ce41592
Lasze <Laszer2710@gmail.com>
2020-04-04 04:19:02 +0200

    update

--- topic.py
+++ topic.py
@@ -1,8 +1,9 @@
 import datetime
 
 class Topic:
-    def __init__(self, name: str, difficulty: int, time_spent: int):
+    def __init__(self, name: str, difficulty: int, time_spent: int, spaced_rep_time: int):
         self.name = name
         self.difficulty = difficulty
         self.time_spent = time_spent
         self.last_done = datetime.datetime.now()
+        self.spaced_rep_time = spaced_rep_time
--- user.py
+++ user.py
@@ -1,17 +1,24 @@
 import calendar
+from datetime import datetime
 
 class User:
-    def __init__(self, name: str):
+    def __init__(self, name: str, start_time: datetime, end_time: datetime):
         self.name = name
         self.points = 0
         self.calendar = calendar
         self.friendlist = []
+        
+        self.start_time = start_time
+        self.end_time = end_time
 
     def update_points(self, pts: int):
         self.points += pts
         
-    def add_to_friends(self, user):
-        self.friendlist.append(user)
+    def add_to_friends(self, users):
+        try:
+            self.friendlist.extend(users)
+        except TypeError:
+            self.friendlist.append(users)
 
 class Ranking:
     def __init__(self, users, sort=False):
